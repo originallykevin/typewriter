8084445d52ac74b69c0940e78b2e7a43c8f65119
originallykevin <ly1@ualberta.ca>
2022-08-30 22:39:06 +0000

    format file.

--- typewriter.js
+++ typewriter.js
@@ -1,8 +1,8 @@
 const sentence = 'hello there from lighthouselabs' + '\n';
 
-let count = 0
+let count = 0;
 for (const char of sentence) {
   setTimeout((letter) => {
     process.stdout.write(char);
-  }, count += 50)
+  }, count += 50);
 }
